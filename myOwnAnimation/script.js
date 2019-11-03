16397d08f5208c08563b5adb6aef0b99334138fa
Roman Kostiuk <roman89ua@gmail.com>
2019-11-03 20:23:02 +0200

    total changed code. New code. Not done yet.

--- myOwnAnimation/script.js
+++ myOwnAnimation/script.js
@@ -1,33 +1,90 @@
-let xLine = 10;
-let yLine = 10;
-let a = setInterval(moveRight, 20);
+let xLine = 0,
+    yLine = 0,
+    step = 25,
+    a, b, c, d;
+let toMove = document.getElementById('move');
+
+
 
 function moveRight() {
 
-    $('#move').offset({ left: xLine });
-    $('#move').offset({ top: yLine });
-
-    xLine++;
-    let bb = setTimeout(moveDown, 4000);
+    toMove.style.marginLeft = xLine + "px";
     if (xLine > 200) {
         clearInterval(a);
+        b = setInterval(moveDown, step);
     }
+    xLine++;
 }
 
 function moveDown() {
-    $('#move').offset({ top: yLine });
+    toMove.style.marginTop = yLine + 'px';
+    if (yLine > 200) {
+        clearInterval(b);
+        c = setInterval(moveLeft, step);
+    }
     yLine++;
-    let cc = setTimeout(moveLeft, 4000);
 }
 
 function moveLeft() {
-    $('#move').offset({ left: xLine });
+    toMove.style.marginLeft = xLine + "px";
+    if (xLine <= 0) {
+        clearInterval(c);
+        d = setInterval(moveUp, step);
+    }
     xLine--;
-    let dd = setTimeout(moveUp, 4000);
 }
 
 function moveUp() {
-    $('#move').offset({ top: yLine });
+    toMove.style.marginTop = yLine + 'px';
+    if (yLine <= 0) {
+        clearInterval(d);
+        a = setInterval(moveRight, step);
+    }
     yLine--;
-    let aa = setTimeout(moveRight, 4000);
 }
+
+document.getElementById('start').onclick = function() {
+    a = setInterval(moveRight, step);
+}
+
+let stopButton = document.getElementById('stop').onclick = function() {
+    clearInterval(a);
+    clearInterval(b);
+    clearInterval(c);
+    clearInterval(d);
+}
+
+// document.getElementById('move').onclick = function() {
+
+//     if (a = setInterval(moveRight, step)) {
+//         clearInterval(a);
+//         step = step - 10;
+//         a = setInterval(moveRight, step);
+//         if (step < 20) {
+//             alert('You are win!!!!Congrets!!!!');
+//         }
+//     } else if (b = setInterval(moveDown, step)) {
+//         clearInterval(b);
+//         step = step - 10;
+//         b = setInterval(moveDown, step);
+//         if (step < 20) {
+//             alert('You are win!!!!Congrets!!!!');
+//         }
+//     } else if (c = setInterval(moveLeft, step)) {
+//         clearInterval(c);
+//         step = step - 10;
+//         c = setInterval(moveLeft, step);
+//         if (step < 20) {
+//             alert('You are win!!!!Congrets!!!!');
+//         }
+//     } else if (d = setInterval(moveUp, step)) {
+//         clearInterval(d);
+//         step = step - 10;
+//         d = setInterval(moveUp, step);
+//         if (step < 20) {
+//             alert('You are win!!!!Congrets!!!!');
+//         }
+
+//     }
+
+// }
